817d561baee3aa72529a6961c83331cf1c2331a8
codze-fullstack <ahmed.codze@gmail.com>
2021-06-23 02:23:49 +0200

    'features'

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,5 +32,48 @@ $(document).ready(function(){
     } else {
         $('.main-carousel .carousel-inner img').height( ( $(window).height() - $('nav').height() ) / 1.5);
     }
+
+    // products swiper
+
+    const swiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 1,
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {  
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+              },
+              // when window width is >= 480px
+              480: {
+                slidesPerView: 2,
+                spaceBetween: 30
+              },
+              // when window width is >= 640px
+              800: {
+                slidesPerView: 3,
+                spaceBetween: 20
+              },
+              1000: {
+                slidesPerView: 4,
+                spaceBetween: 20
+              }
+            },
+        
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
 }); 
 
